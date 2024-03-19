e050ea5c3ec5afc8fd9593ce84d544ac6f6a36bb
Umar Ali <umeralikhan7864@gmail.com>
2024-03-20 03:26:22 +0500

    text

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:gap-8 py-8 items-center lg:py-16 lg:px-16">
       <div className="py-12 px-8 lg:px-8 rounded-lg flex flex-col justify-center md:block">
-        <h2 className="text-6xl bg-gradient-to-r font-bold from-blue-400 via-green-600 bg-clip-text text-transparent">
+        <h2 className="text-4xl lg:text-6xl bg-gradient-to-r font-bold from-blue-400 via-green-600 bg-clip-text text-transparent">
           Become A Certified Web 3.0 <br /> And Metaverse Developer
         </h2>
         <div>
